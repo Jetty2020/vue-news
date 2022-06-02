da341eb768424319ad785c35fdcdbee494d88f4b
Jetty <jetty_park@gccompany.co.kr>
2022-06-02 21:23:41 +0900

    :lipstick: Ask, Jobs 리스트 스타일 수정

--- src/views/AskView.vue
+++ src/views/AskView.vue
@@ -1,9 +1,25 @@
 <template>
   <div>
-    <p v-for="item in fetchedAsk" :key="item.id">
-      <router-link :to="`/item/${item.id}`">{{ item.title }}</router-link>
-      <small>{{ item.time_ago }} by {{ item.user }}</small>
-    </p>
+    <ul class="news-list">
+      <li class="post" v-for="item in fetchedAsk" :key="item.id">
+        <div class="points">
+          {{ item.points }}
+        </div>
+        <div>
+          <p class="news-title">
+            <router-link :to="`/item/${item.id}`">
+              {{ item.title }}
+            </router-link>
+          </p>
+          <small class="link-text">
+            {{ item.time_ago }} by
+            <router-link :to="`/user/${item.user}`">
+              {{ item.user }}
+            </router-link>
+          </small>
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -20,4 +36,28 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+.news-list {
+  display: list-item;
+}
+.post {
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid #eee;
+  list-style: none;
+}
+.points {
+  width: 80px;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #42b883;
+}
+.news-title {
+  margin: 0;
+}
+.link-text {
+  color: #828282;
+}
+</style>
--- src/views/JobsView.vue
+++ src/views/JobsView.vue
@@ -1,9 +1,25 @@
 <template>
   <div>
-    <p v-for="job in this.$store.state.jobs" :key="job.id">
-      <a :href="job.url">{{ job.title }}</a>
-      <small>{{job.time_ago}}, {{ job.domain }}</small>
-    </p>
+    <ul class="news-list">
+      <li class="post" v-for="item in this.$store.state.jobs" :key="item.id">
+        <div class="points">
+          {{ item.points || 0 }}
+        </div>
+        <div>
+          <p class="news-title">
+            <a :href="item.url">
+              {{ item.title }}
+            </a>
+          </p>
+          <small class="link-text">
+            {{ item.time_ago }},
+            <a :href="item.url">
+              {{ item.domain }}
+            </a>
+          </small>
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -15,4 +31,28 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+.news-list {
+  display: list-item;
+}
+.post {
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid #eee;
+  list-style: none;
+}
+.points {
+  width: 80px;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #42b883;
+}
+.news-title {
+  margin: 0;
+}
+.link-text {
+  color: #828282;
+}
+</style>
