5468c1b62a6460a5628b61451cf63f267572dac2
Jetty <jetty_park@gccompany.co.kr>
2022-06-02 17:03:55 +0900

    :recycle: store의 actions, mutations 속성 모듈화

---
+++ src/store/actions.js
@@ -0,0 +1,31 @@
+import { fetchNewsList, fetchJobsList, fetchAskList } from '../api/index';
+
+export default {
+  FETCH_NEWS({ commit }) {
+    fetchNewsList()
+      .then(({ data }) => {
+        commit('SET_NEWS', data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  },
+  FETCH_JOBS({ commit }) {
+    fetchJobsList()
+      .then(({ data }) => {
+        commit('SET_JOBS', data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  },
+  FETCH_ASK({ commit }) {
+    fetchAskList()
+      .then(({ data }) => {
+        commit('SET_ASK', data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  },
+};
--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import { createStore } from 'vuex';
-import { fetchNewsList, fetchJobsList, fetchAskList } from '../api/index';
+import mutations from './mutations';
+import actions from './actions';
 
 export const store = createStore({
   state: {
@@ -7,49 +8,11 @@ export const store = createStore({
     jobs: [],
     ask: [],
   },
-  mutations: {
-    SET_NEWS(state, news) {
-      state.news = news;
-    },
-    SET_JOBS(state, jobs) {
-      state.jobs = jobs;
-    },
-    SET_ASK(state, ask) {
-      state.ask = ask;
-    },
-  },
+  mutations,
   getters: {
     fetchedAsk(state) {
       return state.ask;
     },
   },
-  actions: {
-    FETCH_NEWS({ commit }) {
-      fetchNewsList()
-        .then(({ data }) => {
-          commit('SET_NEWS', data);
-        })
-        .catch((err) => {
-          console.error(err);
-        });
-    },
-    FETCH_JOBS({ commit }) {
-      fetchJobsList()
-        .then(({ data }) => {
-          commit('SET_JOBS', data);
-        })
-        .catch((err) => {
-          console.error(err);
-        });
-    },
-    FETCH_ASK({ commit }) {
-      fetchAskList()
-        .then(({ data }) => {
-          commit('SET_ASK', data);
-        })
-        .catch((err) => {
-          console.error(err);
-        });
-    },
-  },
+  actions,
 });
