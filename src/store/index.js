89a1551fa2baf666e729d0a69357f7f1c7c0c4d3
Jetty <jetty_park@gccompany.co.kr>
2022-06-02 16:41:28 +0900

    :sparkles: News 페이지 vuex store 연결

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,25 @@
 import { createStore } from 'vuex';
+import { fetchNewsList } from '../api/index';
 
 export const store = createStore({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    news: [],
+  },
+  mutations: {
+    SET_NEWS(state, news) {
+      state.news = news;
+    }
+  },
+  actions: {
+    FETCH_NEWS(ctx) {
+      fetchNewsList()
+        .then((res) => {
+          console.log(res.data);
+          ctx.commit('SET_NEWS', res.data);
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    },
+  },
 });
--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -1,23 +1,13 @@
 <template>
-  <div v-for="user in users" :key="user.id">{{ user.title }}</div>
+  <div v-for="user in this.$store.state.news" :key="user.id">
+    {{ user.title }}
+  </div>
 </template>
 
 <script>
-import { fetchNewsList } from '../api/index';
-
 export default {
-  data() {
-    return {
-      users: [],
-    };
-  },
   created() {
-    fetchNewsList()
-      .then((res) => {
-        this.users = res.data;
-        console.log(res.data);
-      })
-      .catch((err) => console.error(err));
+    this.$store.dispatch('FETCH_NEWS');
   },
 };
 </script>
