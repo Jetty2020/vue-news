8957e32ea6c030214b1a9c05c760ff7bdc990d73
Jetty <jetty_park@gccompany.co.kr>
2022-06-02 21:15:41 +0900

    :lipstick: News 리스트 스타일 수정

--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -1,14 +1,32 @@
 <template>
   <div>
-    <p v-for="item in this.$store.state.news" :key="item.id">
-      <a :href="item.url">{{ item.title }}</a>
-      <small
-        >{{ item.time_ago }} by
-        <router-link :to="`/user/${item.user}`">{{
-          item.user
-        }}</router-link></small
-      >
-    </p>
+    <ul class="news-list">
+      <li class="post" v-for="item in this.$store.state.news" :key="item.id">
+        <div class="points">
+          {{ item.points }}
+        </div>
+        <div>
+          <p class="news-title">
+            <a :href="item.url">
+              {{ item.title }}
+            </a>
+          </p>
+          <small class="link-text">
+            by
+            <router-link :to="`/user/${item.user}`">
+              {{ item.user }}
+            </router-link>
+          </small>
+        </div>
+        <!-- <a :href="item.url"></a>
+        <small
+          >{{ item.time_ago }} by
+          <router-link :to="`/user/${item.user}`">{{
+            item.user
+          }}</router-link></small
+        > -->
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -20,4 +38,28 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+.news-list {
+  display: list-item;
+}
+.post {
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid #eee;
+  list-style: none;
+}
+.points {
+  width: 80px;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #42b883;
+}
+.news-title {
+  margin: 0;
+}
+.link-text {
+  color: #828282;
+}
+</style>
