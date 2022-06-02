0178e8dec92837de829d691e5031a096a758b395
Jetty <jetty_park@gccompany.co.kr>
2022-06-02 21:02:44 +0900

    :sparkles: Page transition 구현

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <tool-bar></tool-bar>
-  <router-view />
+  <transition name="page">
+    <router-view />
+  </transition>
 </template>
 
 <script>
@@ -17,5 +19,14 @@ export default {
 body {
   padding: 0;
   margin: 0;
+}
+/* Router transition */
+.page-enter-active,
+.page-leave-active {
+  transition: opacity 0.5s ease;
+}
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
 }
 </style>
